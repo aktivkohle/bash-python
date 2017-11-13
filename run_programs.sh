7c0358ae6ff4ec5a85393ad2e8ddfdc7abde3fdf
felix <felix.s@posteo.de>
2017-11-13 13:28:51 +0100

    now it tells you the status with which the program ended, usually a 0 if all is good or a 1 otherwise

--- run_programs.sh
+++ run_programs.sh
@@ -8,6 +8,7 @@ date -u >> $logfile
 echo now running say_hello.py >> $logfile
 
 {  time python say_hello.py >> $logfile 2>&1; } 2>> $logfile
+echo That process exited with status $? >> $logfile
 
 echo -en '\n' >> $logfile # (skips a line)
 
@@ -22,9 +23,11 @@ timeout 10 python take_too_long.py >> $logfile 2>&1
 if [ $? -eq 124 ]; then
     # Timeout occurred
     echo take_too_long.py has exceeded 10 seconds >> $logfile
+    echo That process exited with status $? >> $logfile
 else
     # No hang
     echo take_too_long.py has run to completion >> $logfile
+    echo That process exited with status $? >> $logfile
 fi
 
 echo -en '\n' >> $logfile
@@ -33,6 +36,7 @@ echo -en '\n' >> $logfile
 echo now running do_something_else.py >> $logfile
 
 {  time python do_something_else.py >> $logfile 2>&1; } 2>> $logfile
+echo That process exited with status $? >> $logfile
 
 echo -en '\n' >> $logfile
 
