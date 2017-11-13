98e458fea26516818130ed51ba343663a29d99b0
felix <felix.s@posteo.de>
2017-11-13 13:07:40 +0100

    file name of logfile is now stored in a variable set at the start of the program

--- run_programs.sh
+++ run_programs.sh
@@ -1,38 +1,41 @@
-echo -en '\n' >> output.log # (skips a line)
+# no spacese around the equals sign when defining variables in bash!
+logfile='output.log'
 
-date -u >> output.log
+echo -en '\n' >> $logfile # (skips a line)
 
-echo now running say_hello.py >> output.log
+date -u >> $logfile
 
-{  time python say_hello.py >> output.log 2>&1; } 2>> output.log
+echo now running say_hello.py >> $logfile
 
-echo -en '\n' >> output.log # (skips a line)
+{  time python say_hello.py >> $logfile 2>&1; } 2>> $logfile
 
-date -u >> output.log
+echo -en '\n' >> $logfile # (skips a line)
 
-echo -en '\n' >> output.log
+date -u >> $logfile
+
+echo -en '\n' >> $logfile
 
 # in the following, 10 are seconds to timeout, 124 is a code for if it times out
 # http://www.gnu.org/software/coreutils/manual/html_node/timeout-invocation.html
 
-timeout 10 python take_too_long.py >> output.log 2>&1
+timeout 10 python take_too_long.py >> $logfile 2>&1
 if [ $? -eq 124 ]; then
     # Timeout occurred
-    echo take_too_long.py has exceeded 10 seconds >> output.log
+    echo take_too_long.py has exceeded 10 seconds >> $logfile
 else
     # No hang
-    echo take_too_long.py has run to completion >> output.log
+    echo take_too_long.py has run to completion >> $logfile
 fi
 
-echo -en '\n' >> output.log
+echo -en '\n' >> $logfile
 
 
-echo now running do_something_else.py >> output.log
+echo now running do_something_else.py >> $logfile
 
-{  time python do_something_else.py >> output.log 2>&1; } 2>> output.log
+{  time python do_something_else.py >> $logfile 2>&1; } 2>> $logfile
 
-echo -en '\n' >> output.log
+echo -en '\n' >> $logfile
 
-date -u >> output.log
+date -u >> $logfile
 
-echo -en '\n' >> output.log
+echo -en '\n' >> $logfile
